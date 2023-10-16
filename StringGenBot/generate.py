aef9f41dd4a9e34540ff1cfebbf26303b42835a1
Ahmed programmer <119659334+qithoniq@users.noreply.github.com>
2023-10-16 15:55:31 +0300

    Update generate.py

--- StringGenBot/generate.py
+++ StringGenBot/generate.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram1 import Client as Client1
 from asyncio.exceptions import TimeoutError
 from telethon.sessions import StringSession
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import KeyboardMarkup, KeyboardButton
 from pyrogram.errors import (
     ApiIdInvalid,
     PhoneNumberInvalid,
